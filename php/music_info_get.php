4d86186675dcf2704d4c5dd3fb83b09a28816122
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2021-06-02 21:41:30 +0800

    Select 0 & 2 from the same path

--- php/music_info_get.php
+++ php/music_info_get.php
@@ -2,7 +2,7 @@
 
 function get_music_source ($song_id, $type = 0, $root = false) {
   $root = $root ? __DIR__ . '/../' : '';
-  $base_path = $root . ($type == 0 ? 'slbackups_mlicqx83sq/Music' : "music/$song_id");
+  $base_path = $root . ($type == 1 ? "music/$song_id" : 'slbackups_mlicqx83sq/Music');
   $full_path = "$base_path/$song_id.*";
   $file_list = glob($full_path);
   $file_name = '';
@@ -20,7 +20,7 @@ function get_music_source ($song_id, $type = 0, $root = false) {
 
 function get_art_work ($song_id, $type = 0, $root = false) {
   $root = $root ? __DIR__ . '/../' : '';
-  $full_path = $root . ($type == 0 ? "slbackups_mlicqx83sq/Music/$song_id.*" : "music/$song_id/cover.*");
+  $full_path = $root . ($type == 1 ? "music/$song_id/cover.*" : "slbackups_mlicqx83sq/Music/$song_id.*");
   $file_list = glob($full_path);
   $file_name = 'assets/images/users/pre-radion.jpg';
 
