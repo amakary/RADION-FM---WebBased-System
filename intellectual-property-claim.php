d263ddee64d7f57e3a39869b5d9e7402559e7201
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2021-07-14 16:16:35 +0800

    Update file

--- intellectual-property-claim.php
+++ intellectual-property-claim.php
@@ -1,62 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>        
+    <head>
         <!-- META SECTION -->
-        <title>Intellectual Property Claim</title>            
+        <title>Intellectual Property Claim</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
+
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
         <!-- END META SECTION -->
-        
-        <!-- CSS INCLUDE -->        
+
+        <!-- CSS INCLUDE -->
         <link rel="stylesheet" type="text/css" id="theme" href="css/theme-default.css"/>
-        <!-- EOF CSS INCLUDE -->                
+        <!-- EOF CSS INCLUDE -->
 <link rel="stylesheet" type="text/css" href="radio/radio.css"/>
         <!-- EOF CSS INCLUDE -->
-		
+
 		<!-- RADIO ANIMATION -->
 		<script type="text/javascript" src="js/howler.js"></script>
-		<script type="text/javascript" src="radio/radio.js"></script>		
+		<script type="text/javascript" src="radio/radio.js"></script>
     </head>
     <body>
         <div class="error-container">
-            <div align="center"><img src="logo-sda.png"></div>
-            <div class="error-text">Intellectual Property Claim</div>	
+            <div align="center"><img src="img/letsgo.png"></div>
+            <div class="error-text">Intellectual Property Policy</div>
 <br>
 <br>
 <div class="error-subtext">Find Us in Social Media!</div>
             <div class="row">
                 <div class="col-md-12" align="center">
-                    <span><a href="https://www.facebook.com/radion.project/" target="blank">Facebook</a></span><span style="padding-right:10px; padding-left:10px;">|</span><span><a href="https://web.telegram.org/#/im?p=@radion_project" target="blank">Telegram</a></span><span style="padding-right:10px; padding-left:10px;">|</span><span><a href="https://twitter.com/RADION_Project" target="blank">Twitter</a></span>
+                    <span><a href="https://www.youtube.com/channel/UCuJOeoT-2o2stPXXJJGzAdg" target="blank">YouTube</a></span><span style="padding-right:10px; padding-left:10px;">|</span><span><a href="https://t.me/joinchat/TdHnxqPrcJPK3mZ1" target="blank">Telegram</a></span><span style="padding-right:10px; padding-left:10px;">|</span><span><a href="https://twitter.com/fm_radion" target="blank">Twitter</a></span>
                 </div>
             </div>
 <hr>
-<p align="justify">radion.fm's content is based on User Generated Content (UGC). RADION does not check user uploaded/created content for violations of copyright or other rights. 
-However, if you believe any of the uploaded content violates your copyright or a related exclusive right, you should follow the process below. RADION looks into reported violations 
-and removes or disables content shown to be violating third party rights.
+<div align="right">Last updated: July 3, 2021</div><br><br>
+<p align="justify"><strong>RADION respects the intellectual property rights of others, and we expect you to do the same. RADION's Terms of Service and Community Guidelines do not allow posting,
+  sharing, or sending any content that violates or infringes someone else’s copyrights, trademarks or other intellectual property rights.</strong></p>
+<br>
+<h3>Copyrights</h3>
+<p align="justify">Copyright is a legal right that protects original works of authorship (e.g., music, composition, beat, etc.). Generally, copyright protects an original expression of an idea
+(e.g., the specific way a music is expressed or created) but does not protect underlying ideas or facts.</p><br>
+
+<h3>Copyright Infringement</h3>
+<p align="justify">We do not allow any content that infringes copyright. The use of copyrighted content of others without proper authorization or legally valid reason may lead to a violation of RADION's
+  policies.<br><br>
+At the same time, not all unauthorized uses of copyrighted content constitute an infringement. In many countries, exceptions to copyright infringement allow the use of copyrighted
+works under certain circumstances without authorization. These include the fair use doctrine in the United States and permitted acts of fair dealing in the European Union (and other
+equivalent exceptions under applicable local laws in other countries).</p><br><br>
+<h3>Removal of Content; Suspension or Termination of Account</h3>
+<p align="justify">Any user content that infringes an other person’s copyright may be removed. The account may be suspended or terminated for multiple copyright violations in connection with the use
+  of the RADION site or app, or other violations of the Terms of Service and Community Guidelines. We reserve the right to refuse any account holder whose account was used for
+  improper activities from opening a new account on RADION’s site or app, or otherwise hosted by RADION.</p>
 <br>
 <br>
-In order to allow us to review your report promptly and effectively, a copyright infringement notice ("Notice") should include the following:
-<br>
-<ol>- Identification of your copyrighted work and what is protected under the copyright(s) that you are referring to</ol>
-<ol>- Your copyright certificate(s)/designation(s) and the type, e.g., registered or unregistered</ol>
-<ol>- Proof of your copyrights ownership, such as the registration number or a copy of the registration certificate</ol>
-<ol>- A short description of how our user(s) allegedly infringe(s) your copyright(s)</ol>
-</p>
+</div>
 
-
-
-
-            
-        </div>
-		
 	</body>
 </html>
-
-
-
-
-
-
