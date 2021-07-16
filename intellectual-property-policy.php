b0180c1f9b2f8eac7df429e12bfac4b35398050f
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2021-07-16 12:03:35 +0800

    Add `intellectual-property-policy.php`

---
+++ intellectual-property-policy.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- META SECTION -->
+        <title>Intellectual Property Policy</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <!-- END META SECTION -->
+
+        <!-- CSS INCLUDE -->
+        <link rel="stylesheet" type="text/css" id="theme" href="css/theme-default.css"/>
+        <!-- EOF CSS INCLUDE -->
+<link rel="stylesheet" type="text/css" href="radio/radio.css"/>
+        <!-- EOF CSS INCLUDE -->
+
+		<!-- RADIO ANIMATION -->
+		<script type="text/javascript" src="js/howler.js"></script>
+		<script type="text/javascript" src="radio/radio.js"></script>
+    </head>
+    <body>
+        <div class="error-container">
+            <div align="center"><img src="img/letsgo.png"></div>
+            <div class="error-text">Intellectual Property Policy</div>
+<br>
+<br>
+<div class="error-subtext">Find Us in Social Media!</div>
+            <div class="row">
+                <div class="col-md-12" align="center">
+                    <span><a href="https://www.youtube.com/channel/UCuJOeoT-2o2stPXXJJGzAdg" target="blank">YouTube</a></span><span style="padding-right:10px; padding-left:10px;">|</span><span><a href="https://t.me/joinchat/TdHnxqPrcJPK3mZ1" target="blank">Telegram</a></span><span style="padding-right:10px; padding-left:10px;">|</span><span><a href="https://twitter.com/fm_radion" target="blank">Twitter</a></span>
+                </div>
+            </div>
+<hr>
+<div align="right">Last updated: July 3, 2021</div><br><br>
+<p align="justify"><strong>RADION respects the intellectual property rights of others, and we expect you to do the same. RADION's Terms of Service and Community Guidelines do not allow posting,
+  sharing, or sending any content that violates or infringes someone else’s copyrights, trademarks or other intellectual property rights.</strong></p>
+<br>
+<h3>Copyrights</h3>
+<p align="justify">Copyright is a legal right that protects original works of authorship (e.g., music, composition, beat, etc.). Generally, copyright protects an original expression of an idea
+(e.g., the specific way a music is expressed or created) but does not protect underlying ideas or facts.</p><br>
+
+<h3>Copyright Infringement</h3>
+<p align="justify">We do not allow any content that infringes copyright. The use of copyrighted content of others without proper authorization or legally valid reason may lead to a violation of RADION's
+  policies.<br><br>
+At the same time, not all unauthorized uses of copyrighted content constitute an infringement. In many countries, exceptions to copyright infringement allow the use of copyrighted
+works under certain circumstances without authorization. These include the fair use doctrine in the United States and permitted acts of fair dealing in the European Union (and other
+equivalent exceptions under applicable local laws in other countries).</p><br><br>
+<h3>Removal of Content; Suspension or Termination of Account</h3>
+<p align="justify">Any user content that infringes an other person’s copyright may be removed. The account may be suspended or terminated for multiple copyright violations in connection with the use
+  of the RADION site or app, or other violations of the Terms of Service and Community Guidelines. We reserve the right to refuse any account holder whose account was used for
+  improper activities from opening a new account on RADION’s site or app, or otherwise hosted by RADION.</p>
+<br>
+<br>
+</div>
+
+	</body>
+</html>
