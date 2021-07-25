bb3ae7226f0df3beb308e9d59241e212e9b15cec
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2021-07-25 14:13:31 +0800

    Update `marketplace.php`

--- marketplace.php
+++ marketplace.php
@@ -142,60 +142,50 @@ require_once 'php/music_info_get.php';
 
             </div>
         </li>
-            <li class="xn-openable">
-            <a href="#"><lord-icon
-    src="https://cdn.lordicon.com//mmspidej.json"
-    trigger="click" target="a"
-    colors="primary:#ffffff,secondary:#F39C12"
-    style="width:45px;height:45px">
-</lord-icon><span class="xn-text">&nbsp;&nbsp; MUSIC</span></a>
-  <ul>
-                <li><a href="submission.php"><span class="xn-text"><i class="fad fa-upload fa-lg"></i>&nbsp;&nbsp; UPLOAD MP3 TO STREAM</span></a></li>
-                <li><a href="mint.php"><span class="xn-text"><i class="fas fa-award fa-lg"></i> &nbsp;&nbsp; NFT FOR EXCLUSIVE RIGHTS</span></a></li>
-                  <li><a href="mint-2.php"><span class="xn-text"><i class="fad fa-sack-dollar fa-lg"></i> &nbsp;&nbsp; MINT NFT TO SELL YOUR MUSIC</span></a></li>
-            </ul>
-        </li>
-
         <li class="xn-openable">
-            <a href="#"><lord-icon
-    src="https://cdn.lordicon.com//hciqteio.json"
-    trigger="click" target="a"
-    colors="primary:#ffffff,secondary:#F39C12"
-    style="width:45px;height:45px">
-</lord-icon> <span class="xn-text">&nbsp;&nbsp; DISCOVER MUSIC</span></a>
-  <ul>
-                <li><a href="marketplace.php"><span class="xn-text"><i class="fad fa-poll-people fa-lg"></i>&nbsp;&nbsp; VOTE ROOM</span></a></li>
-                <li><a href="NFT-music-marketplace-tezos.php"><span class="xn-text"><i class="fas fa-album-collection fa-lg"></i>&nbsp;&nbsp; NFT MARKETPLACE FOR MUSIC</span></a></li>
-            </ul>
-        </li>
+        <a href="#">
+          <lord-icon src="https://cdn.lordicon.com//mmspidej.json" trigger="click" target="a" colors="primary:#ffffff,secondary:#F39C12" style="width:45px;height:45px"></lord-icon>
+          <span class="xn-text">&nbsp;&nbsp; MUSIC</span>
+        </a>
+        <ul>
+            <li><a href="submission.php"><span class="xn-text"><i class="fad fa-upload fa-lg"></i>&nbsp;&nbsp; UPLOAD FILE</span></a></li>
+            <li><a href="mint-NFT-song-track.php"><span class="xn-text"><i class="fad fa-compact-disc fa-lg"></i> &nbsp;&nbsp; CREATE A NFT SONG TRACK</span></a></li>
+        </ul>
+    </li>
 
-        <li class="xn-openable">
-            <a href="#"><lord-icon
-    src="https://cdn.lordicon.com//dizvjgip.json"
-    trigger="click" target="a"
-    colors="primary:#ffffff,secondary:#F39C12"
-    style="width:45px;height:45px">
-</lord-icon> <span class="xn-text">&nbsp;&nbsp; RADION SERVICES</span></a>
-            <ul>
-    <li><a href="ad-submission.php"><span class="xn-text"><i class="fad fa-ad fa-lg"></i>&nbsp;&nbsp; CREATE AD</span></a></li>
-    <li><a href="#"><span class="xn-text"><i class="fal fa-fingerprint fa-lg"></i>&nbsp;&nbsp; ISRC ASSIGNMENT &nbsp;&nbsp;&nbsp;<label class="label label-warning"> IN PROGRESS</label></span></a></li>
+    <li class="xn-openable">
+        <a href="#">
+          <lord-icon src="https://cdn.lordicon.com//hciqteio.json" trigger="click" target="a" colors="primary:#ffffff,secondary:#F39C12" style="width:45px;height:45px"></lord-icon>
+          <span class="xn-text">&nbsp;&nbsp; DISCOVER MUSIC</span>
+        </a>
+        <ul>
+            <li><a href="marketplace.php"><span class="xn-text"><i class="fad fa-poll-people fa-lg"></i>&nbsp;&nbsp; VOTE ROOM</span></a></li>
+            <li><a href="NFT-music-marketplace-tezos.php"><span class="xn-text"><i class="fas fa-album-collection fa-lg"></i>&nbsp;&nbsp; NFT MARKETPLACE FOR MUSIC</span></a></li>
+        </ul>
+    </li>
 
-            </ul>
-        </li>
+    <li class="xn-openable">
+    <a href="#">
+      <lord-icon src="https://cdn.lordicon.com//dizvjgip.json" trigger="click" target="a" colors="primary:#ffffff,secondary:#F39C12" style="width:45px;height:45px"></lord-icon>
+      <span class="xn-text">&nbsp;&nbsp; RADION SERVICES</span>
+    </a>
+        <ul>
+          <li><a href="ad-submission.php"><span class="xn-text"><i class="far fa-ad fa-lg"></i>&nbsp;&nbsp; CREATE AD WITH CRYPTO</span></a></li>
+          <li><a href="#"><span class="xn-text"><i class="fal fa-fingerprint fa-lg"></i>&nbsp;&nbsp; ISRC ASSIGNMENT &nbsp;&nbsp;&nbsp;<label class="label label-primary"> SOON</label></span></a></li>
+          <li><a href="#"><span class="xn-text"><i class="fad fa-chart-network fa-lg"></i>&nbsp;&nbsp; MUSIC SYNC LICENSING &nbsp;&nbsp;&nbsp;<label class="label label-primary"> SOON</label></span></a></li>
+        </ul>
+    </li>
 
 <li class="xn-openable">
-            <a href="submission.php"><lord-icon
-    src="https://cdn.lordicon.com//zqxcrgvd.json"
-    trigger="click" target="a"
-    colors="primary:#ffffff,secondary:#F39C12"
-    style="width:45px;height:45px">
-</lord-icon> <span class="xn-text">&nbsp;&nbsp; LAB</span></a>
-            <ul>
-                <li><a href="#"><span class="xn-text"><i class="fad fa-sack-dollar fa-lg"></i>&nbsp;&nbsp; MUSIC SYNC LICENSING &nbsp;&nbsp;&nbsp;<label class="label label-warning"> IN PROGRESS</label></span></a></li>
-                <li><a href="#"><span class="xn-text"><i class="fad fa-sack-dollar fa-lg"></i>&nbsp;&nbsp; MECHANICAL ROYALTIES &nbsp;&nbsp;&nbsp;<label class="label label-warning"> IN PROGRESS</label></span></a></li>
-    <li><a href="#"><span class="xn-text"><i class="fad fa-sack-dollar fa-lg"></i>&nbsp;&nbsp; CRYPTO BILLBOARD CHART &nbsp;&nbsp;&nbsp;<label class="label label-warning"> IN PROGRESS</label></span></a></li>
-  </ul>
-        </li>
+<a href="submission.php">
+  <lord-icon src="https://cdn.lordicon.com//zqxcrgvd.json" trigger="click" target="a" colors="primary:#ffffff,secondary:#F39C12" style="width:45px;height:45px"></lord-icon>
+  <span class="xn-text">&nbsp;&nbsp; LAB</span>
+</a>
+<ul>
+  <li><a href="#"><span class="xn-text"><i class="fad fa-sack-dollar fa-lg"></i>&nbsp;&nbsp; MECHANICAL ROYALTIES &nbsp;&nbsp;&nbsp;<label class="label label-warning"> IN PROGRESS</label></span></a></li>
+  <li><a href="#"><span class="xn-text"><i class="fad fa-sack-dollar fa-lg"></i>&nbsp;&nbsp; CRYPTO BILLBOARD &nbsp;&nbsp;&nbsp;<label class="label label-warning"> IN PROGRESS</label></span></a></li>
+</ul>
+    </li>
 
         </ul>
         <!-- END X-NAVIGATION -->
@@ -278,83 +268,82 @@ require_once 'php/music_info_get.php';
         <!-- START WIDGETS -->
         <div class="row">
 
-            <div class="col-md-3">
+          <div class="col-md-3">
+            <!-- START WIDGET MESSAGES -->
+            <div class="widget widget-primary widget-item-icon">
+              <div class="widget-item-left">
+                <img src="img/ON-logo.png" style="height:40px; width:40px;">
+              </div>
+              <div class="widget-data">
+                <div class="widget-int num-count">TOKEN</div>
+                <div class="widget-title">BALANCE</div>
+                <div class="widget-subtitle radio-balance" style="color:#F39C12;">0 RADIO</div>
+              </div>
+            </div>
+            <!-- END WIDGET MESSAGES -->
+          </div>
 
-                <!-- START WIDGET MESSAGES -->
-                <div class="widget widget-primary widget-item-icon" onclick="#">
+          <div class="col-md-3">
+            <!-- START WIDGET MESSAGES -->
+            <div class="widget widget-primary widget-item-icon">
                     <div class="widget-item-left">
                         <div style="height:40px; width:40px; margin-left:10px;">
                             <svg xmlns="http://www.w3.org/2000/svg" width="47" height="64" viewBox="0 0 47 64">
                                 <path style="fill:#737577;" d="M30.252 63.441c-4.55 0-7.864-1.089-9.946-3.267-2.08-2.177-3.121-4.525-3.121-7.041 0-.92.181-1.694.544-2.323a3.993 3.993
-        0 0 1 1.489-1.489c.629-.363 1.403-.544 2.323-.544.92 0 1.693.181 2.323.544.629.363 1.125.86 1.488 1.489.363.629.544 1.403.544
-        2.323 0 1.113-.266 2.02-.798 2.722-.533.702-1.162 1.161-1.888 1.38.63.87 1.622 1.487 2.977 1.85 1.355.388 2.71.581 4.065.581
-        1.887 0 3.593-.508 5.118-1.524 1.524-1.017 2.65-2.517 3.376-4.501.726-1.984 1.089-4.235 1.089-6.752
-        0-2.734-.4-5.07-1.198-7.005-.775-1.96-1.924-3.412-3.449-4.356a9.21 9.21 0 0 0-4.936-1.415c-1.162 0-2.613.484-4.356
-        1.452l-3.194 1.597v-1.597L37.076 16.4H17.185v19.89c0 1.646.363 3.001 1.089 4.066s1.839 1.597 3.34 1.597c1.16 0 2.274-.387
-        3.339-1.162a11.803 11.803 0 0 0 2.758-2.83c.097-.219.218-.376.363-.473a.723.723 0 0 1 .472-.181c.266 0 .58.133.944.4.339.386.508.834.508
-        1.342a9.243 9.243 0 0 1-.182 1.017c-.822 1.839-1.96 3.242-3.412 4.21a8.457 8.457 0 0
-        1-4.79 1.452c-4.308 0-7.285-.847-8.93-2.54-1.645-1.695-2.468-3.994-2.468-6.897V16.4H.052v-3.703h10.164v-8.42L7.893
-        1.952V.066h6.751l2.54 1.306v11.325l26.28-.072 2.614 2.613-16.116 16.116a10.807 10.807 0 0 1 3.049-.726c1.742
-        0 3.702.557 5.88 1.67 2.202 1.089 3.896 2.59 5.081 4.5 1.186 1.888 1.948 3.703 2.287 5.445.363 1.743.545 3.291.545
-        4.646 0 3.098-.654 5.977-1.96 8.64-1.307 2.661-3.291 4.645-5.953 5.952-2.662 1.307-5.542 1.96-8.639 1.96z"></path>
+0 0 1 1.489-1.489c.629-.363 1.403-.544 2.323-.544.92 0 1.693.181 2.323.544.629.363 1.125.86 1.488 1.489.363.629.544 1.403.544
+2.323 0 1.113-.266 2.02-.798 2.722-.533.702-1.162 1.161-1.888 1.38.63.87 1.622 1.487 2.977 1.85 1.355.388 2.71.581 4.065.581
+1.887 0 3.593-.508 5.118-1.524 1.524-1.017 2.65-2.517 3.376-4.501.726-1.984 1.089-4.235 1.089-6.752
+0-2.734-.4-5.07-1.198-7.005-.775-1.96-1.924-3.412-3.449-4.356a9.21 9.21 0 0 0-4.936-1.415c-1.162 0-2.613.484-4.356
+1.452l-3.194 1.597v-1.597L37.076 16.4H17.185v19.89c0 1.646.363 3.001 1.089 4.066s1.839 1.597 3.34 1.597c1.16 0 2.274-.387
+3.339-1.162a11.803 11.803 0 0 0 2.758-2.83c.097-.219.218-.376.363-.473a.723.723 0 0 1 .472-.181c.266 0 .58.133.944.4.339.386.508.834.508
+1.342a9.243 9.243 0 0 1-.182 1.017c-.822 1.839-1.96 3.242-3.412 4.21a8.457 8.457 0 0
+1-4.79 1.452c-4.308 0-7.285-.847-8.93-2.54-1.645-1.695-2.468-3.994-2.468-6.897V16.4H.052v-3.703h10.164v-8.42L7.893
+1.952V.066h6.751l2.54 1.306v11.325l26.28-.072 2.614 2.613-16.116 16.116a10.807 10.807 0 0 1 3.049-.726c1.742
+0 3.702.557 5.88 1.67 2.202 1.089 3.896 2.59 5.081 4.5 1.186 1.888 1.948 3.703 2.287 5.445.363 1.743.545 3.291.545
+4.646 0 3.098-.654 5.977-1.96 8.64-1.307 2.661-3.291 4.645-5.953 5.952-2.662 1.307-5.542 1.96-8.639 1.96z"></path>
                             </svg>
                         </div>
                     </div>
                     <div class="widget-data">
                         <div class="widget-int num-count">XTZ</div>
                         <div class="widget-title">BALANCE</div>
-                        <div class="widget-subtitle" id="xtz-balance" style="color:#F39C12;">&#42793;</div>
+                        <div class="widget-subtitle xtz-balance" style="color:#F39C12;">0 <span>&#42793;</span></div>
                     </div>
 
                 </div>
                 <!-- END WIDGET MESSAGES -->
 
             </div>
-            <div class="col-md-3">
 
-                <!-- START WIDGET MESSAGES -->
-                <div class="widget widget-primary widget-item-icon" onclick="#">
-                    <div class="widget-item-left">
-                        <i class="fad fa-sack-dollar fa-3x"></i>
-                    </div>
-                    <div class="widget-data">
-                        <div class="widget-int num-count">USD</div>
-                        <div class="widget-title">BALANCE</div>
-                        <div class="widget-subtitle" style="color:#F39C12;"><i class="fas fa-dollar-sign"></i> <span id="usd-balance"></span></div>
-                    </div>
 
-                </div>
-                <!-- END WIDGET MESSAGES -->
-
-            </div>
             <div class="col-md-6">
 
                 <!-- START WIDGET CLOCK -->
                 <div class="widget widget-primary widget-padding-sm">
                     <div class="widget-controls">
-                        <div style="height:35px; width:35px; margin-bottom:-40px; padding-left:20px;" class="widget-control-left">
+                        <div style="height:35px; width:35px; margin-bottom:-37px; padding-left:20px;" class="widget-control-left">
                             <svg xmlns="http://www.w3.org/2000/svg" width="47" height="64" viewBox="0 0 47 64">
                                 <path style="fill:#7B7D7D;" d="M30.252 63.441c-4.55 0-7.864-1.089-9.946-3.267-2.08-2.177-3.121-4.525-3.121-7.041 0-.92.181-1.694.544-2.323a3.993 3.993
-        0 0 1 1.489-1.489c.629-.363 1.403-.544 2.323-.544.92 0 1.693.181 2.323.544.629.363 1.125.86 1.488 1.489.363.629.544 1.403.544
-        2.323 0 1.113-.266 2.02-.798 2.722-.533.702-1.162 1.161-1.888 1.38.63.87 1.622 1.487 2.977 1.85 1.355.388 2.71.581 4.065.581
-        1.887 0 3.593-.508 5.118-1.524 1.524-1.017 2.65-2.517 3.376-4.501.726-1.984 1.089-4.235 1.089-6.752
-        0-2.734-.4-5.07-1.198-7.005-.775-1.96-1.924-3.412-3.449-4.356a9.21 9.21 0 0 0-4.936-1.415c-1.162 0-2.613.484-4.356
-        1.452l-3.194 1.597v-1.597L37.076 16.4H17.185v19.89c0 1.646.363 3.001 1.089 4.066s1.839 1.597 3.34 1.597c1.16 0 2.274-.387
-        3.339-1.162a11.803 11.803 0 0 0 2.758-2.83c.097-.219.218-.376.363-.473a.723.723 0 0 1 .472-.181c.266 0 .58.133.944.4.339.386.508.834.508
-        1.342a9.243 9.243 0 0 1-.182 1.017c-.822 1.839-1.96 3.242-3.412 4.21a8.457 8.457 0 0
-        1-4.79 1.452c-4.308 0-7.285-.847-8.93-2.54-1.645-1.695-2.468-3.994-2.468-6.897V16.4H.052v-3.703h10.164v-8.42L7.893
-        1.952V.066h6.751l2.54 1.306v11.325l26.28-.072 2.614 2.613-16.116 16.116a10.807 10.807 0 0 1 3.049-.726c1.742
-        0 3.702.557 5.88 1.67 2.202 1.089 3.896 2.59 5.081 4.5 1.186 1.888 1.948 3.703 2.287 5.445.363 1.743.545 3.291.545
-        4.646 0 3.098-.654 5.977-1.96 8.64-1.307 2.661-3.291 4.645-5.953 5.952-2.662 1.307-5.542 1.96-8.639 1.96z"></path>
+0 0 1 1.489-1.489c.629-.363 1.403-.544 2.323-.544.92 0 1.693.181 2.323.544.629.363 1.125.86 1.488 1.489.363.629.544 1.403.544
+2.323 0 1.113-.266 2.02-.798 2.722-.533.702-1.162 1.161-1.888 1.38.63.87 1.622 1.487 2.977 1.85 1.355.388 2.71.581 4.065.581
+1.887 0 3.593-.508 5.118-1.524 1.524-1.017 2.65-2.517 3.376-4.501.726-1.984 1.089-4.235 1.089-6.752
+0-2.734-.4-5.07-1.198-7.005-.775-1.96-1.924-3.412-3.449-4.356a9.21 9.21 0 0 0-4.936-1.415c-1.162 0-2.613.484-4.356
+1.452l-3.194 1.597v-1.597L37.076 16.4H17.185v19.89c0 1.646.363 3.001 1.089 4.066s1.839 1.597 3.34 1.597c1.16 0 2.274-.387
+3.339-1.162a11.803 11.803 0 0 0 2.758-2.83c.097-.219.218-.376.363-.473a.723.723 0 0 1 .472-.181c.266 0 .58.133.944.4.339.386.508.834.508
+1.342a9.243 9.243 0 0 1-.182 1.017c-.822 1.839-1.96 3.242-3.412 4.21a8.457 8.457 0 0
+1-4.79 1.452c-4.308 0-7.285-.847-8.93-2.54-1.645-1.695-2.468-3.994-2.468-6.897V16.4H.052v-3.703h10.164v-8.42L7.893
+1.952V.066h6.751l2.54 1.306v11.325l26.28-.072 2.614 2.613-16.116 16.116a10.807 10.807 0 0 1 3.049-.726c1.742
+0 3.702.557 5.88 1.67 2.202 1.089 3.896 2.59 5.081 4.5 1.186 1.888 1.948 3.703 2.287 5.445.363 1.743.545 3.291.545
+4.646 0 3.098-.654 5.977-1.96 8.64-1.307 2.661-3.291 4.645-5.953 5.952-2.662 1.307-5.542 1.96-8.639 1.96z"></path>
                             </svg>
                         </div>
         <div style="width:100%; margin:5px 20px 0px 40px; color:#7B7D7D;" align="left">
-        <h3><strong style="color:#7B7D7D;">Tezos</strong></h3>
-        <div style="margin-top:-10px;">XTZ/USD</div>
-        </div>
+    <h5><strong style="color:#7B7D7D;">Tezos</strong></h5>
+    <div style="margin-top:-10px; font-size:11px;">XTZ/USD</div>
+    </div>
                     </div>
 
-                    <div class="widget-big-int" style="margin-top:-40px;"><small>$</small><span class="tezos-price-usd"></span></div>
+                    <div class="widget-big-int" style="margin-top:-33px;"><small>$</small><span class="tezos-price-usd"></span></div>
                     <div class="widget-subtitle">Change 24h
                       <span>
                         <span class="tezos-change-24hr-down" style="display:none;">
@@ -371,11 +360,12 @@ require_once 'php/music_info_get.php';
 
 
                             <div align="left" style="margin-top:0px; padding-left:15px;">
+                            <!-- <span><a href="#modal_basic" class="widget-control-right linko" data-toggle="modal" style="font-size:13px; text-decoration:none;"><i class="fad fa-wallet"></i> Connect Wallet</a></span> -->
                             <span><a href="#" id="connect-wallet" class="widget-control-right" style="font-size:13px; text-decoration:none; color:#F39C12;"><i class="fad fa-wallet"></i> Connect Wallet</a></span>
                             <span style="padding-right:3px; padding-left:3px;">|</span>
                             <span><a href="#modal_small" data-toggle="modal" style="font-size:13px; text-decoration:none; color:#F39C12;"><i class="fad fa-qrcode"></i> Receive</a></span>
                             <span style="padding-right:3px; padding-left:3px;">|</span>
-                            <span><a href="#" style="font-size:13px; text-decoration:none; color:#F39C12;"><i class="fad fa-paper-plane"></i> Send</a></span>
+                            <span style="color:#797D7F;">USD Balance <i class="fas fa-dollar-sign"></i> <span class="usd-balance">0</span></span>
           </div>
 
                     </div>
@@ -1051,8 +1041,9 @@ on_link='<a href='+result[3]+' target="_blank" style="text-decoration:none" rel=
     if (connected) {
       await wallet.clearActiveAccount()
       $('#connect-wallet').html('<i class="fad fa-wallet"></i> Connect Wallet')
-      $('#xtz-balance').text('0 ꜩ')
-      $('#usd-balance').text('0')
+      $('.xtz-balance').text('0 ꜩ')
+      $('.usd-balance').text('0')
+      $('.radio-balance').text('0 RADIO')
       $('#get_source').empty()
       connected = false
       return false
@@ -1075,12 +1066,17 @@ on_link='<a href='+result[3]+' target="_blank" style="text-decoration:none" rel=
 
     const address = await wallet.getPKH()
     const mutez = await tezos.tz.getBalance(address)
-    const balance = parseInt(mutez) / 1000000
+    const balance = mutez / 1000000
     const usdBalance = (balance * parseFloat(window.priceUsd)).toFixed(2)
+    const radioBalanceURL = 'https://api.better-call.dev/v1/account/florencenet/' + address + '/token_balances?contract=KT1XLDJzzgSAb6HMXAKoFTxEQ3zMA7HGgU91'
+    const radio = await $.getJSON(radioBalanceURL)
+    const radioBal = radio.balances[0]
+    const radioBalance = typeof radioBal !== 'undefined' ? parseInt(radioBal.balance) / (10 ** radioBal.decimals) : 0
 
     $('#get_source').text(address)
-    $('#xtz-balance').text(balance + ' ꜩ')
-    $('#usd-balance').text(usdBalance)
+    $('.xtz-balance').text(balance + ' ꜩ')
+    $('.usd-balance').text(usdBalance)
+    $('.radio-balance').text(radioBalance + ' RADIO')
   })
 
   async function purchaseSong () {
