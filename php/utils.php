ce2b26917ef472fac766bd4c77654e7b4f99a013
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2021-08-13 21:58:24 +0800

    Add `get_text` function

--- php/utils.php
+++ php/utils.php
@@ -23,6 +23,24 @@ function get_json ($url) {
   return is_json($response) ? json_decode($response) : $response;
 }
 
+function get_text ($url) {
+  $curl = curl_init();
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET'
+  ));
+
+  $response = curl_exec($curl);
+  curl_close($curl);
+
+  return $response;
+}
+
 function deldir ($path) {
   if (!file_exists($path)) return;
 
