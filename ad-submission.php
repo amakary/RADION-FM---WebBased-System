5618b73f3784f39c19b41116165a369ae92d656b
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2021-08-17 14:15:57 +0800

    Update `ad-submission.php`

--- ad-submission.php
+++ ad-submission.php
@@ -229,7 +229,7 @@ require_once 'slpw/sitelokpw.php';
           style="width:45px;height:45px">
       </lord-icon> <span class="xn-text">&nbsp;&nbsp; DISCOVER MUSIC</span></a>
         <ul>
-                      <li><a href="marketplace.php"><span class="xn-text"><i class="fad fa-poll-people fa-lg"></i>&nbsp;&nbsp; VOTE ROOM</span></a></li>
+                      <li><a href="marketplace.php"><span class="xn-text"><i class="fad fa-poll-people fa-lg"></i>&nbsp;&nbsp; ENGAGE WITH NEW TALENTS</span></a></li>
                       <li><a href="NFT-music-marketplace-tezos.php"><span class="xn-text"><i class="fas fa-album-collection fa-lg"></i>&nbsp;&nbsp; NFT MARKETPLACE FOR MUSIC</span></a></li>
                   </ul>
               </li>
