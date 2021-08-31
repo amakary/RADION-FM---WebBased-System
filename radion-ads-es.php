f0c8a353969e749112d4efa60b5430eda69f70ea
RADION <31939627+amakary@users.noreply.github.com>
2021-08-31 11:32:18 -0400

    Add files via upload

---
+++ radion-ads-es.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <!-- META SECTION -->
+        <title>PUBLICIDAD</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+		<link rel="manifest" href="favicon/manifest.json">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+        <!-- END META SECTION -->
+
+        <!-- CSS INCLUDE -->
+        <link rel="stylesheet" type="text/css" id="theme" href="css/theme-dark.css"/>
+		<link rel="stylesheet" type="text/css" href="css/all.css" />
+        <!-- EOF CSS INCLUDE -->
+
+		<script type="text/javascript" src="js/all.js"></script>
+    </head>
+    <body>
+        <div class="error-container" style="padding:20px;">
+            <div class="error-code"><img src="img/logo-test.png" style="width:178px; height:178px;"></div>
+            <div class="error-text">PUBLICIDAD Y COMERCIO</div>
+            <div class="error-subtext">Anuncie en RADION para transmitir su mensaje a través de nuestras comunidades criptográficas. Tenemos la plataforma y los canales para entregar contenido visual y de audio a una audiencia bien orientada que conoce la tecnología blockchain y las criptomonedas. Encuentra el formato adecuado para lograr tu objetivo. No se requiere un gasto mínimo.<br>
+			</div>
+			<div style="color:#2980B9;" align="right"><small><strong>Note</strong>: Transacciones confirmadas en Blockchain.<span></small></div>
+
+			<div class="error-actions" style="border-bottom: 1px solid; border-color:#ABB2B9; margin-bottom:20px;"></div>
+
+			<div align="justify" style="margin-top:30px; margin-bottom:30px; border-bottom: 1px solid; border-color:#ABB2B9;">
+			<div style="margin-top:30px;"><i class="fal fa-bullseye-arrow fa-4x"></i></div>
+			<div style="padding-left:60px; margin-top:-35px;"><h3>PUBLICIDAD EN PANCARTA:</h3></div>
+			<div style="padding-top:20px; padding-bottom:30px; color:#aaa;">RADIONERS utiliza nuestra plataforma para enviar, compartir y descubrir música y podcasts con otros. Mensajes breves para llegar a la audiencia o para correr la voz; funciona muy bien para la participación interactiva. Los mensajes personales se pueden enviar a través del panel de control y la página principal.</div>
+			</div>
+
+			<div align="justify" style="margin-top:30px; margin-bottom:30px; border-bottom: 1px solid; border-color:#ABB2B9;">
+			<div style="margin-top:30px;"><i class="fad fa-audio-description fa-4x"></i></div>
+			<div style="padding-left:60px; margin-top:-35px;"><h3>DESCRIPCION EN AUDIO:</h3></div>
+			<div style="padding-top:20px; padding-bottom:30px; color:#aaa;">La descripción de audio es una excelente manera de distribuir contenido especifico para una audiencia que prefiere el contenido de audio a los artículos. ¿Quiere enviar una descripción de audio de su evento criptográfico, proyecto STO o DeFi? ¡No dude en hacerlo! Estaremos encantados de ayudarle en su empresa ...</div>
+			</div>
+
+			<div align="justify" style="margin-top:30px; margin-bottom:30px; border-bottom: 1px solid; border-color:#ABB2B9;">
+			<div style="margin-top:30px;"><i class="fad fa-signal-stream fa-4x"></i></div>
+			<div style="padding-left:65px; margin-top:-35px;"><h3>PUBLICIDAD EN AUDIO:</h3></div>
+			<div style="padding-top:20px; padding-bottom:30px; color:#aaa;">Publicidad en audio es una excelente manera de cruzar la red con un mensaje entre canciones o podcast.</div>
+			</div>
+
+			<div align="justify" style="margin-top:30px; margin-bottom:30px; border-bottom: 1px solid; border-color:#ABB2B9;">
+			<div style="margin-top:30px;"><i class="fad fa-code fa-4x"></i></div>
+			<div style="padding-left:70px; margin-top:-35px;"><h3>PUBLICIDAD DE PORTADA:</h3></div>
+			<div style="padding-top:20px; padding-bottom:30px; color:#aaa;">¡Usted toma el control de nuestra portada el tiempo que quiera! Si desea que nuestro visitante vea su mensaje
+tan pronto como nos visiten? ¡Esta es la mejor manera de difundir su mensaje!.</div>
+			</div>
+
+			<div align="justify" style="margin-top:30px; margin-bottom:30px; border-bottom: 1px solid; border-color:#ABB2B9;">
+			<div style="margin-top:30px;"><i class="fas fa-podcast fa-4x"></i></div>
+			<div style="padding-left:60px; margin-top:-35px;"><h3>PUBLICIDAD EN PODCAST:</h3></div>
+			<div style="padding-top:20px; padding-bottom:30px; color:#aaa;">La publicidad de podcast se explican por sí mismos, sin embargo, a diferencia de los comerciales de audio en la transmisión, su podcaster favorito es el que entregará su mensaje en su propio podcast.</div>
+
+        </div>
+		<div align="center" style="padding-bottom:50px;"><img src="img/logo-dark.png" style="width:75px; height:25px;"></div>
+    </body>
+</html>
